2964024dcd10851b15bb14cd2c7073eefbf23bff
Alexandrp2 <undercoverword@gmail.com>
2021-02-09 10:19:46 +0100

    Remove ununsed classes

--- src/main/java/com/gostyle/webservice/dao/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gostyle.webservice.dao;
-
-import com.gostyle.webservice.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
